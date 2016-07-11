516fb98f212ddb5b858efddec6423a93f70d50de
touta <marvette.feghali@elementn.com>
2016-07-11 15:09:16 +0300

    ISS Tracking

--- scripts/Maps/html/live.html.js
+++ scripts/Maps/html/live.html.js
@@ -3,7 +3,7 @@
 write=nobody
 execute=anonymous 
   **/ 
- /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<!DOCTYPE html>\n<html>\n  <head>\n    <title>Live Map</title>\n    <meta name=\"viewport\" content=\"initial-scale=1.0\">\n    <meta charset=\"utf-8\">\n\t<link rel=\"shortcut icon\"type=\"image/x-icon\" href=\"data:image/x-icon;,\">\n\t<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script>\n    \n     <!--REPLACE marv.scriptrapps.io WITH YOUR SCRIPTR DOMAIN -->\n\t<link rel=\"stylesheet\" href=\"https://marv.scriptrapps.io/Maps/css/style.css\" />\n    <script src=\"https://marv.scriptrapps.io/Maps/static/config.js\"></script> \n    <script src=\"https://marv.scriptrapps.io/Maps/static/wsClient.js\"></script>\n    <script src=\"https://marv.scriptrapps.io/Maps/static/dataHandler.js\"></script>\n    <script src=\"https://marv.scriptrapps.io/Maps/static/utils.js\"></script>\n  </head>\n  <body>\n    <div id=\"map\"></div>\n    <script>\n      var client;\n      var dataHandler;\n      var zoom, lat, lng;\n      $(window).load(function() { \n          client = new $.WSClient({config: window.config.websocket});\n          dataHandler = new $.DataHandler({config: window.config.map});\n        \n          zoom = $.urlParam('zoom');\n          lat = $.urlParam('lat');\n          lng = $.urlParam('lng');\n        \n          //Load google Maps API\n          var scr = document.createElement(\"script\");\n          scr.setAttribute(\"src\",'https://maps.googleapis.com/maps/api/js?key='+config.map.key+'&callback=initMap&output='+config.map.output);\n          document.head.appendChild(scr);\n      });\n      \n      var map;\n      \n      var path = [];\n      \n      var isCenterFetched = window.config.map.fetchCenter; \n      \n      function addMapPoints(entries) {\n        map = eval(\"dataHandler.\" + window.config.dataHandler.transformFnc +\"(entries, map)\");\n      }\n\n      function showFailMessage(message) {\n        $(\"#map\").html(message);\n      }\n\n      function initMap() {\n         map = new google.maps.Map(document.getElementById('map'), {\n        \tzoom: (zoom !=null) ? parseInt(zoom) : window.config.map.zoom,\n          \tcenter: (lat !=null && lng != null)? {lat: parseFloat(lat), lng: parseFloat(lng) }  : window.config.map.defaultCenter,\n          \tmapTypeId: eval(window.config.map.type)\n      \t});\n        client.openConnection(addMapPoints, showFailMessage);\n      }\n    </script>\n  </body>\n</html>"},"scriptrdata":[]}}*#*#*/
+ /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"<!DOCTYPE html>\n<html>\n  <head>\n    <title>Live Map</title>\n    <meta name=\"viewport\" content=\"initial-scale=1.0\">\n    <meta charset=\"utf-8\">\n\t<link rel=\"shortcut icon\"type=\"image/x-icon\" href=\"data:image/x-icon;,\">\n\t<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script>\n    \n     <!--REPLACE marv.scriptrapps.io WITH YOUR SCRIPTR DOMAIN -->\n\t<link rel=\"stylesheet\" href=\"https://marv.scriptrapps.io/Maps/css/style.css\" />\n    <script src=\"https://marv.scriptrapps.io/Maps/static/config.js\"></script> \n    <script src=\"https://marv.scriptrapps.io/Maps/static/wsClient.js\"></script>\n    <script src=\"https://marv.scriptrapps.io/Maps/static/httpClient.js\"></script>\n    <script src=\"https://marv.scriptrapps.io/Maps/static/dataHandler.js\"></script>\n    <script src=\"https://marv.scriptrapps.io/Maps/static/utils.js\"></script>\n  </head>\n  <body>\n    <div id=\"map\"></div>\n    <script>\n      var wsClient;\n      var httpClient;\n      var dataHandler;\n      var zoom, lat, lng;\n      $(window).load(function() { \n          wsClient = new $.WSClient({config: window.config.websocket});\n          httpClient = new $.HTTPClient({config: window.config.http});\n          dataHandler = new $.DataHandler({config: window.config.map});\n        \n          zoom = $.urlParam('zoom');\n          lat = $.urlParam('lat');\n          lng = $.urlParam('lng');\n        \n          //Load google Maps API\n          var scr = document.createElement(\"script\");\n          scr.setAttribute(\"src\",'https://maps.googleapis.com/maps/api/js?key='+config.map.key+'&callback=initMap&output='+config.map.output);\n          document.head.appendChild(scr);\n      });\n      \n      var map;\n      var isCenterFetched = window.config.map.fetchCenter; \n      \n      function addMapPoints(entries) {\n        var  map = eval(\"dataHandler.\" + window.config.dataHandler.transformFnc +\"(entries, map)\");\n      }\n\n      function showFailMessage(message) {\n        $(\"#map\").html(message);\n      }\n\n      function initMap() {\n         httpClient.callApi(httpClient.buildRequest(), openSocketConnection, showFailMessage);\n      }\n      \n      function openSocketConnection(entries) {\n         var center =  entries[0];\n         map = new google.maps.Map(document.getElementById('map'), {\n        \tzoom: (zoom !=null) ? parseInt(zoom) : window.config.map.zoom,\n          \tcenter: center,\n          \tmapTypeId: eval(window.config.map.type)\n      \t });\n         addMapPoints(entries);\n         wsClient.openConnection(addMapPoints, showFailMessage);\n      }\n    </script>\n  </body>\n</html>"},"scriptrdata":[]}}*#*#*/
 var content= '<!DOCTYPE html>\n\
 <html>\n\
   <head>\n\
@@ -17,17 +17,20 @@ var content= '<!DOCTYPE html>\n\
 	<link rel=\"stylesheet\" href=\"https://marv.scriptrapps.io/Maps/css/style.css\" />\n\
     <script src=\"https://marv.scriptrapps.io/Maps/static/config.js\"></script> \n\
     <script src=\"https://marv.scriptrapps.io/Maps/static/wsClient.js\"></script>\n\
+    <script src=\"https://marv.scriptrapps.io/Maps/static/httpClient.js\"></script>\n\
     <script src=\"https://marv.scriptrapps.io/Maps/static/dataHandler.js\"></script>\n\
     <script src=\"https://marv.scriptrapps.io/Maps/static/utils.js\"></script>\n\
   </head>\n\
   <body>\n\
     <div id=\"map\"></div>\n\
     <script>\n\
-      var client;\n\
+      var wsClient;\n\
+      var httpClient;\n\
       var dataHandler;\n\
       var zoom, lat, lng;\n\
       $(window).load(function() { \n\
-          client = new $.WSClient({config: window.config.websocket});\n\
+          wsClient = new $.WSClient({config: window.config.websocket});\n\
+          httpClient = new $.HTTPClient({config: window.config.http});\n\
           dataHandler = new $.DataHandler({config: window.config.map});\n\
         \n\
           zoom = $.urlParam(\'zoom\');\n\
@@ -41,13 +44,10 @@ var content= '<!DOCTYPE html>\n\
       });\n\
       \n\
       var map;\n\
-      \n\
-      var path = [];\n\
-      \n\
       var isCenterFetched = window.config.map.fetchCenter; \n\
       \n\
       function addMapPoints(entries) {\n\
-        map = eval(\"dataHandler.\" + window.config.dataHandler.transformFnc +\"(entries, map)\");\n\
+        var  map = eval(\"dataHandler.\" + window.config.dataHandler.transformFnc +\"(entries, map)\");\n\
       }\n\
 \n\
       function showFailMessage(message) {\n\
@@ -55,12 +55,18 @@ var content= '<!DOCTYPE html>\n\
       }\n\
 \n\
       function initMap() {\n\
+         httpClient.callApi(httpClient.buildRequest(), openSocketConnection, showFailMessage);\n\
+      }\n\
+      \n\
+      function openSocketConnection(entries) {\n\
+         var center =  entries[0];\n\
          map = new google.maps.Map(document.getElementById(\'map\'), {\n\
         	zoom: (zoom !=null) ? parseInt(zoom) : window.config.map.zoom,\n\
-          	center: (lat !=null && lng != null)? {lat: parseFloat(lat), lng: parseFloat(lng) }  : window.config.map.defaultCenter,\n\
+          	center: center,\n\
           	mapTypeId: eval(window.config.map.type)\n\
-      	});\n\
-        client.openConnection(addMapPoints, showFailMessage);\n\
+      	 });\n\
+         addMapPoints(entries);\n\
+         wsClient.openConnection(addMapPoints, showFailMessage);\n\
       }\n\
     </script>\n\
   </body>\n\
--- scripts/Maps/static/config.js.js
+++ scripts/Maps/static/config.js.js
@@ -3,34 +3,32 @@
 write=nobody
 execute=anonymous 
   **/ 
- /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"//Configuration file used by client side\nvar config = {\n  http: {\n    baseUrl: \"https://marv.scriptrapps.io\", //Your scriptr domain\n  \tdataScript:\"Maps/scripts/generateStaticData\" //Scriptr script responsible for fetching needed geolocation data\n  },\n  websocket: {\n    url: \"wss://api.scriptrapps.io/YOUR_ANONYMOUS_TOKEN\",\n    receive_channel: \"pushRandomData\"//The Channel name to which the live GPS data will be pushed, and to which the client websocket will subscribe\n  },\n  map: {\n  \tkey: \"GOOGLE_MAPS_BROWSER_API_KEY\", //https://developers.google.com/maps/documentation/javascript/get-api-key#get-an-api-key\n    output: 'embed',\n\ttype: \"google.maps.MapTypeId.ROADMAP\", //MapTypeId.ROADMAP, MapTypeId.SATELLITE, MapTypeId.HYBRID, MapTypeId.TERRAIN \n    mode: [\"marker\", \"polyline\"],\n    strokestrokeColor: '#FF0000',\n    strokeOpacity: 1.0,\n    strokeWeight: 2,\n    zoom: 14,\n    fetchCenter: true,\n    defaultCenter: {lat: 32.7038888889, lng: -117.153611111},\n\tblueImage: 'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Azure-icon.png',\n    pinkImage: 'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Pink-icon.png',\n  \tgreenImage: 'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Chartreuse-icon.png'\n  },\n  dataHandler: {\n    transformFnc:  \"renderRandomGeoLocations\" // //Define the transformation function you will use. ex: \"renderEarthquakeGeoLocations\", \"renderRandomGeoLocations\" \n  }\n}\n\n"},"scriptrdata":[]}}*#*#*/
+ /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"//Configuration file used by client side\nvar config = {\n  http: {\n    baseUrl: \"https://marv.scriptrapps.io\", //Your scriptr domain\n  \tdataScript:\"Maps/scripts/generateIssLiveData\" //Scriptr script responsible for fetching needed geolocation data\n  },\n  websocket: {\n    url: \"wss://api.scriptrapps.io/RTg2MTczN0ZDRQ==\",\n    receive_channel: \"ISSGeoLocation\"//The Channel name to which the live GPS data will be pushed, and to which the client websocket will subscribe\n  },\n  map: {\n  \tkey: \"AIzaSyBcPYghFh_BXz4dDz-TXTHbU2iV3Wbf57I\", //https://developers.google.com/maps/documentation/javascript/get-api-key#get-an-api-key\n    output: 'embed',\n\ttype: \"google.maps.MapTypeId.HYBRID\", //MapTypeId.ROADMAP, MapTypeId.SATELLITE, MapTypeId.HYBRID, MapTypeId.TERRAIN \n    mode: [\"marker\", \"polyline\"], // flags to be used by the dataHandler transformFnc\n    strokeColor: '#FF0000',\n    strokeOpacity: 2.0, \n    strokeWeight: 2,\n    zoom: 2,\n    fetchCenter: true,\n    defaultCenter: {lat: 32.7038888889, lng: -117.153611111},\n    spaceStationIcon: 'http://icons.iconarchive.com/icons/aha-soft/free-global-security/48/Satellite-icon.png'\n  },\n  dataHandler: {\n    transformFnc:  \"addSpaceStationPoint\" // //Define the transformation function you will use. ex: \"renderEarthquakeGeoLocations\", \"renderRandomGeoLocations\" \n  }\n}\n\n"},"scriptrdata":[]}}*#*#*/
 var content= '//Configuration file used by client side\n\
 var config = {\n\
   http: {\n\
     baseUrl: \"https://marv.scriptrapps.io\", //Your scriptr domain\n\
-  	dataScript:\"Maps/scripts/generateStaticData\" //Scriptr script responsible for fetching needed geolocation data\n\
+  	dataScript:\"Maps/scripts/generateIssLiveData\" //Scriptr script responsible for fetching needed geolocation data\n\
   },\n\
   websocket: {\n\
-    url: \"wss://api.scriptrapps.io/YOUR_ANONYMOUS_TOKEN\",\n\
-    receive_channel: \"pushRandomData\"//The Channel name to which the live GPS data will be pushed, and to which the client websocket will subscribe\n\
+    url: \"wss://api.scriptrapps.io/RTg2MTczN0ZDRQ==\",\n\
+    receive_channel: \"ISSGeoLocation\"//The Channel name to which the live GPS data will be pushed, and to which the client websocket will subscribe\n\
   },\n\
   map: {\n\
-  	key: \"GOOGLE_MAPS_BROWSER_API_KEY\", //https://developers.google.com/maps/documentation/javascript/get-api-key#get-an-api-key\n\
+  	key: \"AIzaSyBcPYghFh_BXz4dDz-TXTHbU2iV3Wbf57I\", //https://developers.google.com/maps/documentation/javascript/get-api-key#get-an-api-key\n\
     output: \'embed\',\n\
-	type: \"google.maps.MapTypeId.ROADMAP\", //MapTypeId.ROADMAP, MapTypeId.SATELLITE, MapTypeId.HYBRID, MapTypeId.TERRAIN \n\
-    mode: [\"marker\", \"polyline\"],\n\
-    strokestrokeColor: \'#FF0000\',\n\
-    strokeOpacity: 1.0,\n\
+	type: \"google.maps.MapTypeId.HYBRID\", //MapTypeId.ROADMAP, MapTypeId.SATELLITE, MapTypeId.HYBRID, MapTypeId.TERRAIN \n\
+    mode: [\"marker\", \"polyline\"], // flags to be used by the dataHandler transformFnc\n\
+    strokeColor: \'#FF0000\',\n\
+    strokeOpacity: 2.0, \n\
     strokeWeight: 2,\n\
-    zoom: 14,\n\
+    zoom: 2,\n\
     fetchCenter: true,\n\
     defaultCenter: {lat: 32.7038888889, lng: -117.153611111},\n\
-	blueImage: \'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Azure-icon.png\',\n\
-    pinkImage: \'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Pink-icon.png\',\n\
-  	greenImage: \'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Ball-Chartreuse-icon.png\'\n\
+    spaceStationIcon: \'http://icons.iconarchive.com/icons/aha-soft/free-global-security/48/Satellite-icon.png\'\n\
   },\n\
   dataHandler: {\n\
-    transformFnc:  \"renderRandomGeoLocations\" // //Define the transformation function you will use. ex: \"renderEarthquakeGeoLocations\", \"renderRandomGeoLocations\" \n\
+    transformFnc:  \"addSpaceStationPoint\" // //Define the transformation function you will use. ex: \"renderEarthquakeGeoLocations\", \"renderRandomGeoLocations\" \n\
   }\n\
 }\n\
 \n\
